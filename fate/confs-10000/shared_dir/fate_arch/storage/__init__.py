c066214e96fe05e6e224e0b06ccf08ec8a8c956b
Huangxy-Minel <huangxy_minel@foxmail.com>
2021-10-30 16:53:58 +0800

    update data structure

--- fate/confs-10000/shared_dir/fate_arch/storage/__init__.py
+++
@@ -1,6 +0,0 @@
-from fate_arch.storage._types import StorageTableMetaType, StorageEngine
-from fate_arch.storage._types import StandaloneStorageType, EggRollStorageType, HDFSStorageType, MySQLStorageType, FileStorageType, PathStorageType, HiveStorageType, LinkisHiveStorageType
-from fate_arch.storage._types import Relationship
-from fate_arch.storage._types import DEFAULT_ID_DELIMITER
-from fate_arch.storage._session import Session, StorageSessionBase
-from fate_arch.storage._table import StorageTableBase, StorageTableMeta
