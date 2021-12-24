45bd3e8fe959f226ccff5ed525c934b15dc29342
Huangxy-Minel <huangxy_minel@foxmail.com>
2021-12-24 18:23:15 +0800

    update file structure

--- fate/confs-9999/create_swarm.sh
+++
@@ -1,6 +0,0 @@
-IP_MANAGER="172.16.0.160"
-
-docker swarm init --advertise-addr=${IP_MANAGER}
-docker swarm join --token <TOKEN> \
-  --advertise-addr <IP-ADDRESS-OF-WORKER-1> \
-  <IP-ADDRESS-OF-MANAGER>:2377
